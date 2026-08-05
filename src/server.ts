3b622bae479ed0bfd90511bfc259d4ad52b325c2
dane.westley <dane.westley@kainos.com>
2026-08-05 14:36:19 +0100

    Fixed local schema issue

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const port = Number(process.env.PORT) || 3000;
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+	console.log(`Server running on http://localhost:${port}`);
 });
