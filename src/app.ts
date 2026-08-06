c9516ea07edac95861d1dbf678ff8c18d3283ebd
Sean Vial <sean.vial@kainos.com>
2026-08-06 13:38:41 +0100

    Implemented basic frontend registration system with workig endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import path from "node:path";
 import express, { type Request, type Response } from "express";
 import nunjucks from "nunjucks";
+import registrationRoutes from "./routes/registrationRoutes";
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 nunjucks.configure(path.join(__dirname, "views"), {
 	autoescape: true,
@@ -11,9 +15,18 @@ nunjucks.configure(path.join(__dirname, "views"), {
 
 app.set("view engine", "njk");
 
-app.get("/", (_req: Request, res: Response) => {
-	res.render("index", { message: "hello world" });
+app.get("/", (req: Request, res: Response) => {
+	const registered = req.query.registered === "1";
+
+	res.render("index", {
+		message: "hello world",
+		registrationSuccessMessage: registered
+			? "Registration successful. You can now sign in."
+			: undefined,
+	});
 });
+
+app.use("/register", registrationRoutes);
 
 app.get("/health", (_req: Request, res: Response) => {
 	res.json({
