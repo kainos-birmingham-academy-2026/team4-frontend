5405cd2361f9999b232644068571562bce0646e0
dane.westley <dane.westley@kainos.com>
2026-08-06 09:31:29 +0100

    Package updates + Moved tests into folder

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { describe, expect, it } from "vitest";
-import app from "./app";
+import app from "../src/app";
 
 describe("app routes", () => {
 	it("GET / returns the index page", async () => {
