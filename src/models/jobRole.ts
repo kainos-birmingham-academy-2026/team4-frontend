3372db0d3f4a4970ccecdd98f9150ae1aac9d581
Asfur Malik <asfur.malik@kainos.com>
2026-08-10 11:38:06 +0100

    Change JobRole to use getter functions

--- src/models/jobRole.ts
+++ src/models/jobRole.ts
@@ -1,9 +1,41 @@
 export class JobRole {
+	private _roleName: string;
+	private _location: string;
+	private _capability: string;
+	private _band: string;
+	private _closingDate: Date;
+
 	constructor(
-		public readonly roleName: string,
-		public readonly location: string,
-		public readonly capability: string,
-		public readonly band: string,
-		public readonly closingDate: Date,
-	) {}
+		roleName: string,
+		location: string,
+		capability: string,
+		band: string,
+		closingDate: Date,
+	) {
+		this._roleName = roleName;
+		this._location = location;
+		this._capability = capability;
+		this._band = band;
+		this._closingDate = closingDate;
+	}
+
+	public get roleName(): string {
+		return this._roleName;
+	}
+
+	public get location(): string {
+		return this._location;
+	}
+
+	public get capability(): string {
+		return this._capability;
+	}
+
+	public get band(): string {
+		return this._band;
+	}
+
+	public get closingDate(): Date {
+		return this._closingDate;
+	}
 }
