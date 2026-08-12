4b4820fc40704fad69351dd3036285e8ae1b92cd
Asfur Malik <asfur.malik@kainos.com>
2026-08-12 13:23:52 +0100

    Update jobRoleRouter

--- src/routes/jobRoleRouter.ts
+++ src/routes/jobRoleRouter.ts
@@ -7,7 +7,7 @@ const controller = new JobRoleController();
 
 //TODO: separate authRouter and jobRoleRouter
 router.get("/", controller.getHome.bind(controller));
-router.get("/register", controller.getRegister.bind(controller));
+//router.get("/register", controller.getRegister.bind(controller));
 router.use(requireAuth);
 router.get("/job-roles", controller.getJobRoles.bind(controller));
 router.get("/job-roles/:id", controller.getJobRoleDetails.bind(controller));
